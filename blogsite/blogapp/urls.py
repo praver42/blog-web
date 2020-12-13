924354b15aa89724ef7a787e523f62eddb4757f1
praver42 <pravermishra57@gmail.com>
2020-12-13 21:10:23 +0530

    first commit

---
+++ blogsite/blogapp/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path,re_path
+from blogapp import views
+
+urlpatterns = [
+    re_path(r'^$',views.PostListView.as_view(),name='post_list'),
+    re_path(r'^about/$',views.AboutView.as_view(),name='about'),
+    re_path(r'^post/(?P<pk>\d+)$',views.PostDetailView.as_view(),name='post_detail'),
+    re_path(r'^post/new/$',views.CreatePostView.as_view(),name='post_new'),
+    re_path(r'^post/(?P<pk>\d+)/edit/$',views.PostUpadateView.as_view(),name='post_edit'),
+    re_path(r'^post/(?P<pk>\d+)/remove/$',views.PostDeleteView.as_view(),name='post_remove'),
+    re_path(r'^drafts/$',views.DraftListView.as_view(),name='post_draft_list'),
+    re_path(r'^post/(?P<pk>\d+)/comment/$',views.add_comment_to_post,name='add_comment_to_post'),
+    re_path(r'^comment/(?P<pk>\d+)/approve/$',views.comment_approve,name='comment_approve'),
+    re_path(r'^comment/(?P<pk>\d+)/remove/$',views.comment_remove,name='comment_remove'),
+    re_path(r'^post/(?P<pk>\d+)/publish/$',views.post_publish,name='post_publish'),
+
+]
---
+++ blogsite/blogapp/views.py
@@ -0,0 +1,82 @@
+from django.shortcuts import render,get_object_or_404,redirect
+from blogapp.models import Post,Comment
+from django.views.generic import (TemplateView,ListView,DetailView,CreateView,UpdateView,DeleteView)
+from django.contrib.auth.mixins import LoginRequiredMixin
+from blogapp.forms import PostForm,CommentForm
+from django.urls import reverse_lazy
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    
+class PostDetailView(DetailView):
+    model = Post
+
+class CreatePostView(LoginRequiredMixin,CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blogapp/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+class PostUpadateView(LoginRequiredMixin,UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blogapp/post_detail.html'
+    form_class = PostForm
+    model = Post   
+
+class PostDeleteView(LoginRequiredMixin,DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+class DraftListView(LoginRequiredMixin,ListView):
+    login_url = '/login/'
+    redirect_field_name = 'blogapp/post_list.html'
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
+
+
+
+############################
+
+@login_required
+def post_publish(request,pk):
+    post =get_object_or_404(Post,pk=pk)
+    post.publish()
+    return redirect('post_detail',pk=pk)
+
+#@login_required
+def add_comment_to_post(request,pk):
+    post =get_object_or_404(Post,pk=pk)
+    if request.method =='POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request,'blogapp/comment_form.html',{'form':form})        
+
+@login_required
+def comment_approve(request,pk):
+    comment =get_object_or_404(Comment,pk=pk)
+    comment.approve()
+    return redirect('post_detail',pk=comment.post.pk)
+
+@login_required
+def comment_remove(request,pk):
+    comment =get_object_or_404(Comment,pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail',pk=post_pk)
